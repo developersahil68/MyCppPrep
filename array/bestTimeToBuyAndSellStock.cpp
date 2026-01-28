18be36784eb4f8ff87d076ca56734e954bad6a78
developersahil68 <sahil852700@gmail.com>
2026-01-28 19:07:50 +0530

    best time to buy and sell stocks type 1

--- array/bestTimeToBuyAndSellStock.cpp
+++ array/bestTimeToBuyAndSellStock.cpp
@@ -8,6 +8,17 @@ int main()
     vector<int> num = {4 , 2 , 8 , 9 , 6 , 7 };
     int n = num.size();
   
-     
+
+    int minimum = num[0];
+    int profit = 0;
+    int maxProfit = 0;
+
+    for(int i = 1 ; i <n ; i++){
+       profit = num[i] - minimum ;
+       maxProfit = max(profit , maxProfit);
+
+       minimum = min(num[i] , minimum);
+    }
+     cout<<maxProfit;
     return 0;
 };
