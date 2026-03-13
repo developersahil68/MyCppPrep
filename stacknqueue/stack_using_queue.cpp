05f9a47cd25e649b8a06cd6855c376afefd891cf
developersahil68 <sahil852700@gmail.com>
2026-03-13 20:38:13 +0530

    stack using queue

---
+++ stacknqueue/stack_using_queue.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+
+class MyStack {
+        queue<int> q;
+public:
+    MyStack() {
+
+    }
+    
+    void push(int x) {
+       int  s = q.size();
+        q.push(x);
+
+        for(int i = 0 ; i < s ; i++){
+            q.push(q.front());
+            q.pop();
+        }
+
+    }
+    
+    int pop() {
+         int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+     return   q.front();
+    }
+    
+    bool empty() {
+        if(q.size() == 0) return true;
+        return false;
+    }
+};
